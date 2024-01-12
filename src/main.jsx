dc10e9f35638fcf6656fa6f633e174f23136b1ac
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-01-13 10:29:20 +1100

    🎨 improve the structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Router from './routes/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router>
 			<App />
-		</Router>
 	</React.StrictMode>
 );
