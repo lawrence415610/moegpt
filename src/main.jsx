f83436badba92db8fc7426d79ca8a8df97d0c445
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-01-13 10:18:26 +1100

    :construction: Router set

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Router from './routes/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>
+);
