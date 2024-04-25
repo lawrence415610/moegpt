bde7d467bf2f3ec8a17553ae53372c8fef9b09d7
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-04-25 21:01:29 +1000

    Successfully accomplished two cloud functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	env: {
+		node: true,
+		es2018: true,
+	},
+	extends: ['eslint:recommended', 'plugin:node/recommended'],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	rules: {
+		// Add your custom rules here
+	},
+};
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,60 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onRequest } = require('firebase-functions/v2/https');
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+// The Firebase Admin SDK to access Firestore.
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+initializeApp();
+const db = getFirestore();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const getAllTopics = onRequest((req, res) => {
+	const userId = req.query.userId;
+	const topics = db.collection('topics');
+
+	const query = topics.where('userId', '==', userId);
+	query
+		.get()
+		.then((snapshot) => {
+			const data = snapshot.docs.map((doc) => doc.data());
+			res.send(data);
+		})
+		.then((err) => {
+			res.send(err);
+		});
+});
+
+const addNewTopic = onRequest((req, res) => {
+	const { userId, userMsg } = req.body;
+	const gptMsg = 'Hello, how can I help you today?';
+	const newTopic = {
+		userId,
+		updatedAt: new Date().toISOString(),
+		name: userMsg.slice(0, 20),
+	};
+
+	db.collection('topics')
+		.add(newTopic)
+		.then((topicRef) => {
+			db.collection('chats')
+				.add({
+					userMsg,
+					gptMsg,
+					updatedAt: new Date().toISOString(),
+					topicId: topicRef.id,
+				})
+				.then((chatRef) => {
+					res.send({ chatId: chatRef.id, topicId: topicRef.id });
+				})
+				.catch((err) => {
+					res.send(err);
+				});
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+});
+
+module.exports = {
+	getAllTopics,
+	addNewTopic,
+};
